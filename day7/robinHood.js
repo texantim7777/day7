8f60d14e8a7edb9331f5a1aac649f1a751ef3c6e
Eric Siebeneich <eric.siebeneich@codercamps.com>
2015-08-04 13:00:26 -0500

    Added Robin Hood loading script

---
+++ day7/robinHood.js
@@ -0,0 +1,17 @@
+﻿var robinHood;
+
+var reader = new XMLHttpRequest() || new ActiveXObject('MSXML2.XMLHTTP');
+
+function loadFile() {
+    reader.open('get', 'robinhood.txt', true);
+    reader.onreadystatechange = displayContents;
+    reader.send(null);
+}
+
+function displayContents() {
+    if (reader.readyState == 4) {
+        robinHood = reader.responseText;
+    }
+}
+
+loadFile();
